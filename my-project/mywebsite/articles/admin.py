ca68007890a7003b77f3aa8605406716fd9ff413
Zümre Atanak <zumre.atanak@outlook.com>
2025-09-11 15:59:32 +0300

    Delete my-project directory

--- my-project/mywebsite/articles/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Article, articleCategory,articleUser
-
-admin.site.register(Article)
-admin.site.register(articleCategory)
-admin.site.register(articleUser)
--- my-project/mywebsite/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import user_homepage_view
-
-urlpatterns = [
-    path("my-profile/", user_homepage_view, name="my-profile-homepage"),
-]
